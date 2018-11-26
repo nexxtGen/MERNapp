22e1f285cb6c73f71cc4650ff749ed6e6b55d32a
Kamil Szczypior <kszczypior1@gmail.com>
2018-11-26 23:06:25 +0100

    ESJ scheme

---
+++ client/components/ArrowHello.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const ArrowHello = (props, context) => {
+  return (
+  );
+}
+
+ArrowHello.propTypes = {
+};
+
+export default ArrowHello;
